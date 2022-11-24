ef3b9fd52d5f5c7a87383c25b2b2768773c24fb5
angela <nik1955mix@gmail.com>
2022-11-24 21:17:34 +0300

    ex25

---
+++ ex25/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3,5 -> 243 
+double a;
+double b;
+double c;
+
+Console.Write("Введите число A: ");  
+a = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите число B, которое возводит число А в натуральную степень В: ");  
+b = Convert.ToDouble(Console.ReadLine());
+
+c = Math.Pow(a, b);
+
+Console.WriteLine($"{a},{b} -> {c}");  
+
+Console.ReadLine();
+
+
+
